0dc8611778e682d3ab82d94bf89ff453b6aeb75f
Barret Schultz <71146303+bschultz52@users.noreply.github.com>
2020-09-11 13:51:05 -0400

    LAA Master

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
 setup(name='docassemble.TLABarrettLaw',
       version='0.0.1',
       description=('A docassemble extension.'),
-      long_description='# docassemble.TLABarrettLaw\n\nA docassemble extension.\n\n## Author\n\nBarret Schultz, bgschult@ualberta.ca\n\n',
+      long_description='# docassemble.TLABarrettLaw\r\n\r\nA docassemble extension.\r\n\r\n## Author\r\n\r\nBarret Schultz, bgschult@ualberta.ca\r\n\r\n',
       long_description_content_type='text/markdown',
       author='Barret Schultz',
       author_email='bgschult@ualberta.ca',
